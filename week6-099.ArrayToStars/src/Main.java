b9f3bd2aa6e6193cf6637a123a9ecf25ef4ee928
forhad <rhidoy87@yahoo.com>
2020-10-13 17:51:39 +0200

    Add files via upload

---
+++ week6-099.ArrayToStars/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        // test the method here
+        int[] array = {5, 1, 3, 4, 2};
+        printArrayAsStars(array);
+    }
+
+    public static void printArrayAsStars(int[] array) {
+        // write code here
+        
+        for(int i : array){
+            for(int j = 1; j <= i ; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
