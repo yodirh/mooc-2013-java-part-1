45e1b750f3c284f55678de2b219979d6bfaef0e9
forhad <rhidoy87@yahoo.com>
2020-10-13 16:12:07 +0200

    Add files via upload

---
+++ week3-049.LastCharacter/src/LastCharacter.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class LastCharacter {
+
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String name = reader.nextLine();
+        System.out.println(lastCharacter(name));
+    }
+    
+    public static char lastCharacter(String name) {
+        return name.charAt(name.length()-1);
+    }
+}
