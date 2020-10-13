a4e507966811dbabb82037f66254ef8846c414d4
forhad <rhidoy87@yahoo.com>
2020-10-13 15:28:25 +0200

    Add files via upload

---
+++ week2-028.FromHundredToOne/src/FromHundredToOne.java
@@ -0,0 +1,12 @@
+
+public class FromHundredToOne {
+
+    public static void main(String[] args) {
+        // Write your program here
+        int i = 100;
+        while(i >= 1){
+            System.out.println(i);
+            i -- ;
+        }
+    }
+}
