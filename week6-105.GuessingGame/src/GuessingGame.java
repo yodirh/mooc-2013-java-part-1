a4c4f86a1d4a79264eb60f21181b3c7559d14210
forhad <rhidoy87@yahoo.com>
2020-10-13 17:55:37 +0200

    Add files via upload

---
+++ week6-105.GuessingGame/src/GuessingGame.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class GuessingGame {
+
+    private Scanner reader;
+
+    public GuessingGame() {
+        // use only this scanner, othervise the tests do not work
+        this.reader = new Scanner(System.in);
+    }
+
+    public void play(int lowerLimit, int upperLimit) {
+        instructions(lowerLimit, upperLimit);
+
+        // write the guessing logic here
+        
+        int looplimit = howManyTimesHalvable(upperLimit - lowerLimit);
+        for (int i = 0; i<looplimit; i++){
+            if(isGreaterThan(average(lowerLimit, upperLimit))==true){
+                lowerLimit= average(lowerLimit, upperLimit)+1 ;
+            }else{
+                upperLimit = average(lowerLimit, upperLimit);
+            }
+            if(upperLimit==lowerLimit){
+            System.out.println("the number you're thinking of is "+lowerLimit );
+            break;
+        }
+
+        }
+        
+        
+    }
+
+    // implement here the methods isGreaterThan and average
+
+    public void instructions(int lowerLimit, int upperLimit) {
+        int maxQuestions = howManyTimesHalvable(upperLimit - lowerLimit);
+
+        System.out.println("Think of a number between " + lowerLimit + "..." + upperLimit + ".");
+
+        System.out.println("I promise you that I can guess the number you are thinking with " + maxQuestions + " questions.");
+        System.out.println("");
+        System.out.println("Next I'll present you a series of questions. Answer them honestly.");
+        System.out.println("");
+    }
+
+    // a helper method:
+    public static int howManyTimesHalvable(int number) {
+        // we create a base two logarithm  of the given value
+
+        // Below we swap the base number to base two logarithms!
+        return (int) (Math.log(number) / Math.log(2)) + 1;
+    }
+    
+    public boolean isGreaterThan(int value){
+        System.out.println("Is your number greater than "+ value + "? (y/n)");
+        String ans = reader.nextLine();
+        if(ans.equals("y")){
+            return true;
+        }else{
+            return false;
+        } 
+    } 
+    
+    public int average(int firstnumber, int secondnumber){
+         int k = (firstnumber+secondnumber)/ 2;
+        return k;
+    }
+    
+    
+}
---
+++ week6-105.GuessingGame/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        // test your program here
+        GuessingGame game = new GuessingGame();
+        //System.out.println(game.average(6, 12));
+        game.play(1,100);
+        //System.out.println(game.isGreaterThan(32));
+    }    
+}
