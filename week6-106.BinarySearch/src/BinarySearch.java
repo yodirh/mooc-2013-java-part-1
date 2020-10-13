cd4e95b9e3249db53bda5988143c802be32af3b8
forhad <rhidoy87@yahoo.com>
2020-10-13 17:56:00 +0200

    Add files via upload

---
+++ week6-106.BinarySearch/src/BinarySearch.java
@@ -0,0 +1,21 @@
+public class BinarySearch {
+    public static boolean search(int[] array, int searchedValue) {
+        int beginning = 0;
+        int end = array.length - 1;
+
+        while (beginning <= end) {
+            int middle = (beginning + end) / 2;
+            if (array[middle] == searchedValue) {
+                return true;
+            }else if(searchedValue > array[middle]){
+                beginning = middle +1;
+            }else if(searchedValue< array[middle]){
+                end = middle-1;
+            }
+                
+
+            // restrict the search area 
+        }
+        return false;
+    }
+}
