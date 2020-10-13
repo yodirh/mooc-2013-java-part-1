b3e34e69c7b4bf620eac5e19e1a934f68deef2c8
forhad <rhidoy87@yahoo.com>
2020-10-13 16:40:55 +0200

    Add files via upload

---
+++ week3-056.ReversingText/src/ReversingText.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class ReversingText {
+
+    public static String reverse(String text) {
+        // write your code here
+        // note that method does now print anything, it RETURNS the reversed string
+        String reversed= "";
+        int i = text.length();
+        while( i > 0){
+            char j = text.charAt(i -1);
+            i --;
+            reversed = reversed + j;
+        }
+        
+        return reversed;
+    }
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Type in your text: ");
+        String text = reader.nextLine();
+        System.out.println("In reverse order: " + reverse(text));
+    }
+}
